d04234d7405c85e1c0499ec491d3ccdc9a7a21ee
TodFoxBR <davidprofile.acc@gmail.com>
2022-05-14 14:37:46 -0300

    Teste

--- src/main/java/glitch/me/nikatasks/NikaTasks.java
+++ src/main/java/glitch/me/nikatasks/NikaTasks.java
@@ -12,7 +12,7 @@ public class NikaTasks extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().print("Hello, World!");
+        response.getWriter().print("Hello, World!!!!!!!!!!!!!!!!!");
         System.out.println("Testando");
     }
 }
